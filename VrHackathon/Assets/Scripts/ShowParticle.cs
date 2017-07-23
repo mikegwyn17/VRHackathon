5d4706874aa5e53eca4a5badbb8f074ba7e5a663
roggaf <phammytheclimber@gmail.com>
2017-07-22 23:48:05 -0400

    Added scene for physics and particles

---
+++ VrHackathon/Assets/Scripts/ShowParticle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowParticle : MonoBehaviour {	
+	
+      private ParticleSystem _psystem;
+         
+		void Awake() {
+		   _psystem = GetComponent<ParticleSystem>();
+		}
+         
+        void OnTriggerEnter (Collider col)  {
+			_psystem.Play();
+        }
+}
