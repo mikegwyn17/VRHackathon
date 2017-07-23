ada2c4ea78362e51a15bda329fae7caca9deb2a8
Nathan <nvelliquette1@gmail.com>
2017-07-22 23:45:25 -0400

    Add grab functionality and doge material

---
+++ VrHackathon/Assets/Scripts/Grab.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour {
+
+    public float grabRadius;
+    public OVRInput.Controller controller;
+    public LayerMask grabMask;
+    public string buttonName;
+
+    private GameObject grabbedObject;
+    private bool grabbing;
+    private Quaternion lastRotation, currentRotation
+
+    void GrabObject()
+    {
+        grabbing = true;
+
+        RaycastHit[] hits;
+
+        hits = Physics.SphereCastAll(transform.position, grabRadius, transform.forward, 0f, grabMask);
+
+        if (hits.Length > 0)
+        {
+            int closestHit = 0;
+
+            for(var i = 0; i < hits.Length; ++i)
+            {
+                if (hits[i].distance < hits[closestHit].distance)
+                    closestHit = i;
+
+                grabbedObject = hits[closestHit].transform.gameObject;
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+                grabbedObject.transform.position = transform.position;
+                grabbedObject.transform.parent = transform; 
+            }
+        }
+    }
+
+    void DropObject()
+    {
+        grabbing = false;
+
+        if(grabbedObject != null)
+        {
+            grabbedObject.transform.parent = null;
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+
+            grabbedObject.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(controller);
+            grabbedObject.GetComponent<Rigidbody>().angularVelocity = GetAngularVelocity();
+
+            grabbedObject = null;
+        }
+    }
+
+    Vector3 GetAngularVelocity()
+    {
+        Quaternion deltaRotation = currentRotation * Quaternion.Inverse(lastRotation);
+        return new Vector3(Mathf.DeltaAngle(0, deltaRotation.eulerAngles.x), Mathf.DeltaAngle(0, deltaRotation.eulerAngles.y), Mathf.DeltaAngle(0, deltaRotation.eulerAngles.z));
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if(grabbedObject != null)
+        {
+            lastRotation = currentRotation;
+            currentRotation = grabbedObject.transform.rotation;
+        }
+
+        if(Input.GetAxis(buttonName) == 1 && !grabbing)
+            GrabObject();
+
+        if (Input.GetAxis(buttonName) < 1 && grabbing)
+            DropObject();
+
+        		
+	}
+}
