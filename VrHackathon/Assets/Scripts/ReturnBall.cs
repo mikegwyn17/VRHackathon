2891cd5324e77d24b36fc3786cbdf4f8aa1c075f
Nathan <nvelliquette1@gmail.com>
2017-07-23 00:48:14 -0400

    Add return ball script

---
+++ VrHackathon/Assets/Scripts/ReturnBall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnBall : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("ReturnTheFuckingBall") == 1)
+        {
+            transform.position = new Vector3(0, 0.5f, 0);
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+	}
+}
